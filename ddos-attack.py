0450584a8d2e96eafc76513c11c5da2e234867c4
Di4r <119154247+DiarDev@users.noreply.github.com>
2024-06-24 23:03:58 +0700

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -24,7 +24,6 @@ print ("Author   : HA-MRX")
 print ("You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA")
 print ("github   : https://github.com/Ha3MrX")
 print ("Facebook : https://www.facebook.com/muhamad.jabar222")
-print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
